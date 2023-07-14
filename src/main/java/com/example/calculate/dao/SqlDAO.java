26c553bf9cd3049ea7498574515bff0f905e6cde
RamTsk <ramtsk24685@gmail.com>
2023-07-14 09:23:13 +0530

    Adding Servlet Practice with JDBC example

--- src/main/java/com/example/calculate/dao/SqlDAO.java
+++ src/main/java/com/example/calculate/dao/SqlDAO.java
@@ -2,10 +2,12 @@ package com.example.calculate.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class SqlDAO {
     public String sqlProcessDAO(String query,String type) throws Exception{
+        ResultSet rs;
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "SYSTEM", "oracle123")) {
@@ -18,17 +20,29 @@ public class SqlDAO {
                 } else if (type.toLowerCase().equals("create")) {
                     System.out.println("Inside Create-----");
                     boolean status = st.execute(query);
-                    if(status == true) {
+                    if (status == true) {
                         return "Table got Created Successfully";
                     } else {
                         return "Table not Created Successfully";
                     }
+                } else if (type.toLowerCase().equals("select")){
+                    int count=0;
+                    rs = st.executeQuery(query);
+                    while ( rs.next() ) {
+                        count++;
+                        System.out.println(rs.getString("EMPLOYEE_ID"));
+                        System.out.println(rs.getString("EMPLOYEE_NAME"));
+                        System.out.println(rs.getString("CITY"));
+                    }
+                    return count+" no of Rows are fetched...";
                 } else {
                     return "Wrong type of Operation Selected";
                 }
             }
         } catch(Exception e) {
             System.out.println("Exception due to: "+e.toString());
+        } finally {
+            System.out.println("Calling Finally...");
         }
         return null;
     }
