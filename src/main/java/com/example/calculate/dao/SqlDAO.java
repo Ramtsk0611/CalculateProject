d698cd0e02947d069a844a7cf5f4ed763e9b8a6b
PrasannaBharat <aprasannabharathi@gmail.com>
2023-08-18 10:15:27 -0700

    Cookies on the Servlet

--- src/main/java/com/example/calculate/dao/SqlDAO.java
+++ src/main/java/com/example/calculate/dao/SqlDAO.java
@@ -10,7 +10,7 @@ public class SqlDAO {
         ResultSet rs;
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "SYSTEM", "oracle123")) {
+            try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "sys123")) {
                 Statement st = con.createStatement();
                 System.out.println("connection established successfully...!!");
                 if (type.toLowerCase().equals("insert") || type.toLowerCase().equals("update")) {
@@ -19,20 +19,23 @@ public class SqlDAO {
                     return count + " rows got updated/Inserted";
                 } else if (type.toLowerCase().equals("create")) {
                     System.out.println("Inside Create-----");
-                    boolean status = st.execute(query);
-                    if (status == true) {
-                        return "Table got Created Successfully";
-                    } else {
+                    try {
+                        st.execute(query);
+                    } catch(Exception e){
                         return "Table not Created Successfully";
                     }
+                    return "Table got Created Successfully";
                 } else if (type.toLowerCase().equals("select")){
                     int count=0;
                     rs = st.executeQuery(query);
                     while ( rs.next() ) {
                         count++;
-                        System.out.println(rs.getString("EMPLOYEE_ID"));
-                        System.out.println(rs.getString("EMPLOYEE_NAME"));
-                        System.out.println(rs.getString("CITY"));
+//                        System.out.println(rs.getString(0));
+                        System.out.println(rs.getString(1));
+                        System.out.println(rs.getString(2));
+                        System.out.println(rs.getString(3));
+                        System.out.println(rs.getString(4));
+                        System.out.println(rs.getString(5));
                     }
                     return count+" no of Rows are fetched...";
                 } else {
